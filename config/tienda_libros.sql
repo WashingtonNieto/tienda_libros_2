b4023190e32c5f9f9997e1e84fc342b005d84490
washington_nieto <washington_nieto@hotmail.com>
2026-09-18 14:43:32 -0500

    actualizacion bdados

--- config/tienda_libros.sql
+++ config/tienda_libros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-09-2026 a las 21:30:51
+-- Tiempo de generación: 18-09-2026 a las 21:32:42
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
